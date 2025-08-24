70ae85bad53f86c2fc8dfe79dfc0accd4d95373d
Thasleem-MK <mohammedthasleemmk6604@gmail.com>
2025-08-24 14:51:18 +0530

    Navbar Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { Routes } from "./components/Routes";
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-50 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 }

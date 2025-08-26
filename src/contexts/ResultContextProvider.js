51ee7265b2cd5c316f4d73820aca1233bc1f0907
Thasleem-MK <mohammedthasleemmk6604@gmail.com>
2025-08-26 22:12:57 +0530

    Update ResultContextProvider.js

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,18 @@
+const { createContext, useState } = require("react");
+
+const resultCont = createContext();
+// const baseUrl = "https://google-search74.p.rapidapi.com/?query=Nike&limit=10&related_keywords=true";
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+  const [result, setResult] = useState([]);
+  const [isLoading, setIsLoding] = useState(false);
+  const [searchTerms, setSearchTerms] = useState("");
+
+  // /videos, /search, /images
+  const getResult = async (type) => {
+    setIsLoding(true);
+
+    const response = await fetch(`${baseUrl}${type}`);
+  };
+};
